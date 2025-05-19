86e3283a1745750e30872445665062def041d894
idiltkale <idiltkale@gmail.com>
2025-05-19 17:21:53 +0300

    Adjusted initial window size for better layout

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,21 @@ package org.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/view/main_view.fxml"));
-        Scene scene = new Scene(loader.load());
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/org/example/view/main_view.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 1300, 700);
         stage.setTitle("Video Game Catalog");
         stage.setScene(scene);
+        stage.setMinWidth(1000);
+        stage.setMinHeight(600);
         stage.show();
     }
 
